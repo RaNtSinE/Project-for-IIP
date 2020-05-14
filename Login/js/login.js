ee1663ca13eec2d5a7669dcdffe11cb291ad37a1
RaNtSinE <45504704+RaNtSinE@users.noreply.github.com>
2020-05-14 14:47:43 +0700

    login 0.3.5

--- Login/js/login.js
+++ Login/js/login.js
@@ -1,9 +1,11 @@
  setInterval(function () {
             var dataReCaptcha = $("#g-recaptcha-response").val();
             capt = document.getElementsByClassName("g-recaptcha");
+            ent = document.getElementsByClassName("enter");
             if (dataReCaptcha != "" && dataReCaptcha != undefined) {
                 setTimeout(function () {
                     capt[0].classList.add("almostHide");
+                    ent[0].classList.add("enter-visible");
                     setTimeout(function () {
                         capt[0].classList.add("hide");
                     }, 500)
@@ -15,6 +17,7 @@
                     capt[0].classList.remove("hide");
                     setTimeout(function () {
                         capt[0].classList.remove("almostHide");
+                        ent[0].classList.remove("enter-visible");
                     }, 10)
                 }, 100);
             }
